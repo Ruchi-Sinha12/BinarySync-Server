a4100a59499e440163df653112dfa053ae5566fb
Ruchi-Sinha12 <132763682+Ruchi-Sinha12@users.noreply.github.com>
2024-01-26 14:43:19 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
+/********************* COPYRIGHT SSS 2023-2024 ****************************
+ ** MODULE  :  main.cpp
+ **
+ ** PURPOSE :  main file to control the console
+ **
+ ** PROJECT :  Internship Programme
+ **
+ ** HISTORY :  25.12.2023 - Initial Version
+ **
+ ** VERSION :  1.0 
+ **
+ ** AUTHOR  :  Ruchi
+ *************************************************************************/
 
-#include "Client.h"
+#include "Server.h"
 
 int main() 
 {
-    CTcpClient tcpClient(12345);
+	/*
+	 * Create an instance of the CTcpServer class, specifying the port number (12345)
+	 */
+    CTcpServer tcpServer(12345);
 
-    if (!tcpClient.Connect("127.0.0.1")) 
-	{
-        return 1;
-    }
-
-    tcpClient.SendFile("E:\\test.txt");
-    tcpClient.ReceiveResult();
+	/*
+	 * Start listening for incoming client connections
+	 */
+    tcpServer.StartListening();
 
     system("pause");
     return 0;
