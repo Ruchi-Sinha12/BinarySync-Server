0460af23c57f266af514079efa9073e8dfd6e2c9
Ruchi-Sinha12 <132763682+Ruchi-Sinha12@users.noreply.github.com>
2024-01-26 14:57:05 +0530

    Update Server.h

--- Server/Server.h
+++ Server/Server.h
@@ -1,16 +1,3 @@
-/********************* COPYRIGHT SSS 2023-2024 ****************************
- ** MODULE  :  Server.h
- **
- ** PURPOSE :  Declaration for Class CTcpServer
- **
- ** PROJECT :  Internship Programme
- **
- ** HISTORY :  25.12.2023 - Initial Version
- **
- ** VERSION :  1.0 
- **
- ** AUTHOR  :  Ruchi
- *************************************************************************/
 
 /**************************HEADER FILES***********************************/
 
